cdc433937ead5afd027129dbf9ffb59b569f5deb
BiteSnail <rkwoal216@gmail.com>
2024-11-17 03:13:39 +0900

    feat: 아이템 빌리기 및 반납하기

--- backend/beep/src/main/java/org/cosmic/beep/services/ItemService.java
+++ backend/beep/src/main/java/org/cosmic/beep/services/ItemService.java
@@ -3,7 +3,10 @@ package org.cosmic.beep.services;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.cosmic.beep.dtos.ItemDetail;
+import org.cosmic.beep.dtos.RentalDetail;
 import org.cosmic.beep.repositories.ItemRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +17,17 @@ public class ItemService {
 
   public List<ItemDetail> getRentAvailableItems() {
     return ItemDetail.from(itemRepository.findByRentalNull());
+  }
+
+  public List<RentalDetail> getItems(@NonNull String keyword,
+      @NonNull Integer pageNumber,
+      @NonNull Integer pageSize) {
+    return RentalDetail.fromItem(itemRepository.findByNameContainsIgnoreCase(keyword,
+        PageRequest.of(pageNumber, pageSize)));
+  }
+
+  public RentalDetail getItemDetail(@NonNull Long itemId) {
+    return RentalDetail.from(
+        itemRepository.findById(itemId).orElseThrow(IllegalArgumentException::new));
   }
 }
--- backend/beep/src/main/java/org/cosmic/beep/services/RentalService.java
+++ backend/beep/src/main/java/org/cosmic/beep/services/RentalService.java
@@ -1,8 +1,16 @@
 package org.cosmic.beep.services;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.cosmic.beep.dtos.RentalDetail;
+import org.cosmic.beep.entities.Member;
+import org.cosmic.beep.entities.Rental;
+import org.cosmic.beep.entities.RentalLog;
+import org.cosmic.beep.repositories.ItemRepository;
+import org.cosmic.beep.repositories.MemberRepository;
+import org.cosmic.beep.repositories.RentalLogRepository;
 import org.cosmic.beep.repositories.RentalRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -12,8 +20,28 @@ import org.springframework.stereotype.Service;
 public class RentalService {
 
   private final RentalRepository rentalRepository;
+  private final MemberRepository memberRepository;
+  private final ItemRepository itemRepository;
+  private final RentalLogRepository rentalLogRepository;
 
   public List<RentalDetail> getMemberRentals(@NonNull Long memberId) {
     return RentalDetail.from(rentalRepository.findByMember_Id(memberId));
+  }
+
+  @Transactional
+  public void rentalItem(List<Long> itemsId, @NonNull Long memberId) {
+    Member member = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
+    List<Rental> rentals = itemRepository.findByIdIn(itemsId).stream()
+        .map(item -> Rental.from(member, item))
+        .toList();
+    rentalLogRepository.saveAll(rentals.stream().map(RentalLog::from).toList());
+    rentalRepository.saveAll(rentals);
+  }
+
+  @Transactional
+  public void returnItem(List<Long> itemsId) {
+    List<Rental> deleted = rentalRepository.deleteByItem_IdIn(itemsId);
+    rentalLogRepository.findByRentalIn(deleted).forEach(RentalLog::returnItem);
+    rentalRepository.deleteAll();
   }
 }
